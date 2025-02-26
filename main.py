8d06810b797cf36b8c95511e1c6eefe9cbd4ea54
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2025-02-26 12:32:02 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+class Calculadora:
+    
+    def suma(a,b):
+        return 0#a + b
+    
+    
+print( Calculadora.suma(2,3) ) 
+    
+    
+    
---
+++ tests.py
@@ -0,0 +1,5 @@
+from main import Calculadora
+
+
+def test_sums_2_numbers():
+    assert Calculadora.suma(2,3) == 5
